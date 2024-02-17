9840cc569471429672c789781100cf5f3475ffa6
Sydney <sgwcollins@gmail.com>
2024-02-17 14:33:30 -0500

    fixed that

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/v1/:path*",
+        destination: "https://cdp.customer.io/v1/:path*",
+      },
+    ];
+  },
+};
+module.exports = nextConfig;
 
 export default nextConfig;
